4a6f50f41a6d2b5261cfc649c2b31926e75c161c
dvs <dangkhoi23403@gmail.com>
2025-05-02 11:29:39 +0700

    Add files via upload

---
+++ script.php
@@ -0,0 +1,16 @@
+$supercode = data;
+data = ${sever: https://github.com/Khoi-star/nh-y};
+server = https://github.com/Khoi-star/nh-y;
+update = https://github.com/Khoi-star;
+key = $file_exists
+keygen = $file_put_contents
+keygen = $file_get_contents
+keygen = $gd_info
+$parse_ini_file = $false;;;
+$target = $head;
+$headers_list = $table;($parse_str)case;empty-target
+in_array=buff<;>
+$create_function{$system;syslog: true;parse_str:true;;script:$true;move_uploaded_file:$true;dechex:$true;textdomain: headers_list;true: debugger_on;scheme: dba_exists;get_headers: $true;}
+data-gun:{m1887,m1014,mp40,mp5,ump,trogon,mag-7,m590,thompson,m1917,m1973,deserteagle,xm8,ak47,flush-head,m4A1,99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999;}
+data-target:{head};$headers_sent;['push']$ftp_set_option: target: $head;option: target-headers_sent; openal_source_set: setting-controller; codebase: enctype: "ASCII";xdiff_file_merge3: $true;case: text.txt:text.str;plaintext.strike-head;//%#;"0000000000";
+"0";"1"
